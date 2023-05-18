85e5ef747177a3fe34621d895be8bfc4f9dc7ffc
Rose <rosevandergiessen@proton.me>
2023-05-18 11:29:48 +0200

    changed item methods due to build error

--- src/main/java/com/booleanuk/core/Library.java
+++ src/main/java/com/booleanuk/core/Library.java
@@ -18,26 +18,20 @@ public class Library {
     // The following methods may contain code that you are unfamiliar with. The strange syntax of article -> something
     // is called a lambda expression (https://www.w3schools.com/java/java_lambda.asp)
     public String checkInItem(String title) {
-        List<Item> filtered = this.items.stream()
-                .filter(item -> item.title.equals(title))
-                .toList();
-
-        if (filtered.size() < 1) {
-            return "item is not part of the library's collection";
+        for(Item item : this.items){
+            if(item.title.equals(title)){
+                return item.checkIn();
+            }
         }
-
-        return filtered.get(0).checkIn();
+        return "item is not part of the library's collection";
     }
 
     public String checkOutItem(String title) {
-        List<Item> filtered = this.items.stream()
-                .filter(item -> item.title.equals(title))
-                .toList();
-
-        if (filtered.size() < 1) {
-            return "item is not part of the library's collection";
+        for(Item item : this.items){
+            if(item.title.equals(title)){
+                return item.checkOut();
+            }
         }
-
-        return filtered.get(0).checkOut();
+        return "item is not part of the library's collection";
     }
 }
