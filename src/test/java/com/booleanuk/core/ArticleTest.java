02488d0c480814e7804051e396dc31f693b6482c
Dennis <dennisvoutos@gmail.com>
2023-05-18 12:50:54 +0300

    Tests done

--- src/test/java/com/booleanuk/core/ArticleTest.java
+++ src/test/java/com/booleanuk/core/ArticleTest.java
@@ -3,6 +3,8 @@ package com.booleanuk.core;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
 class ArticleTest {
     @Test
     public void shouldCheckOutIfAvailable() {
@@ -31,5 +33,16 @@ class ArticleTest {
         Article article = new Article("JUnit Rocks");
 
         Assertions.assertEquals("item is not currently on loan", article.checkIn());
+    }
+    @Test
+    public void testAuthor(){
+        Article article = new Article("The industrial revolution and it's consequences",new Author("Dennis Voutos"));
+        Assertions.assertEquals("Dennis Voutos",article.author.fullName);
+        Assertions.assertEquals("N/A",article.author.contactInformation);
+        Assertions.assertEquals(0,article.author.age);
+        Book book = new Book("The industrial revolution and it's consequences",new Author("Dennis Voutos","fakemail@gmail.com","Dennisvoutos.gr",25));
+        Assertions.assertEquals("Dennis Voutos",book.author.fullName);
+        Assertions.assertEquals("fakemail@gmail.com",book.author.contactInformation);
+        Assertions.assertEquals(25,book.author.age);
     }
 }
