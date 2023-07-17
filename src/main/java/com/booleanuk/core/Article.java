8cb064f51fc0ef41c42c2b41270021bda1009308
Anna Sperkach <annsperkach@gmail.com>
2023-07-17 11:46:12 +0300

    Change the NewspaperTest and finish the classes with implementing inheritance

--- src/main/java/com/booleanuk/core/Article.java
+++ src/main/java/com/booleanuk/core/Article.java
@@ -1,35 +1,15 @@
 package com.booleanuk.core;
 
-public class Article {
-    String title;
 
-    boolean onLoan = false;
+/*
+There are some classes and tests in the usual place. It's a basic implementation of a library's collection of books, articles and newspapers, with quite a bit of repetition and redundancy.
 
+Refactor the code base using inheritance to remove the repetition
+Modify the existing tests to prove your solution works as intended
+Create a test suite for the Library class, as well as any new classes you create
+ */
+public class Article extends Publication {
     public Article(String title) {
-        this.title = title;
-    }
-
-    public boolean isOnLoan() {
-        return onLoan;
-    }
-
-    public String checkIn() {
-        if (!this.isOnLoan()) {
-            return "item is not currently on loan";
-        }
-
-        this.onLoan = false;
-
-        return "item has been checked in";
-    }
-
-    public String checkOut() {
-        if (this.isOnLoan()) {
-            return "item is currently on loan";
-        }
-
-        this.onLoan = true;
-
-        return "item has been checked out";
+        super(title);
     }
 }
