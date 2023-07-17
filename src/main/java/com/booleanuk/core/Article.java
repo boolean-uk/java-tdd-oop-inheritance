331f838931c90511676c2ba1d529cd5776e9b7e7
Przemyslaw Mroczek <przemyslaw.mroczek@talan.com>
2023-07-17 11:01:50 +0200

    Add Author with tests

--- src/main/java/com/booleanuk/core/Article.java
+++ src/main/java/com/booleanuk/core/Article.java
@@ -1,10 +1,19 @@
 package com.booleanuk.core;
 
-public class Article extends InformationSource{
+public class Article extends InformationSource {
 
+    private Author author;
 
     public Article(String title) {
         super(title);
+
+    }
+    public Article(String title, Author author) {
+        super(title);
+        this.author = author;
     }
 
+    public Author getAuthor() {
+        return author;
+    }
 }
--- src/main/java/com/booleanuk/core/Book.java
+++ src/main/java/com/booleanuk/core/Book.java
@@ -2,8 +2,18 @@ package com.booleanuk.core;
 
 public class Book extends InformationSource {
 
+    private Author author;
+
     public Book(String title) {
         super(title);
     }
 
+    public Book(String title, Author author) {
+        super(title);
+        this.author = author;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
 }
