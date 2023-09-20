8892a48fc8fed485e7b37b8fe661c52324bd1365
Panagiotis Karapiperis <panagiotis_karapiperis@hotmail.com>
2023-09-20 12:33:56 +0300

    Panagiotis Karapiperis ex1

--- src/main/java/com/booleanuk/core/Book.java
+++ src/main/java/com/booleanuk/core/Book.java
@@ -1,35 +1,10 @@
 package com.booleanuk.core;
 
-public class Book {
-    String title;
+import com.booleanuk.core.example.Item;
 
-    boolean onLoan = false;
-
+public class Book extends Item {
     public Book(String title) {
-        this.title = title;
+        super(title);
     }
 
-    public boolean isOnLoan() {
-        return onLoan;
-    }
-
-    public String checkIn() {
-        if (!this.isOnLoan()) {
-            return "item is not currently on loan";
-        }
-
-        this.onLoan = false;
-
-        return "item has been checked in";
-    }
-
-    public String checkOut() {
-        if (this.isOnLoan()) {
-            return "item is currently on loan";
-        }
-
-        this.onLoan = true;
-
-        return "item has been checked out";
-    }
 }
---
+++ src/main/java/com/booleanuk/core/example/Main.java
@@ -0,0 +1,8 @@
+package com.booleanuk.core.example;
+
+public class Main {
+    public static void main(String[] args) {
+        Car BMW = new Car(150);
+        System.out.println(BMW.move());
+    }
+}
