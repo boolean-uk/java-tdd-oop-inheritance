8cbcb6d427dc1e84f0ce93da2942784d03d1334a
Panagiotis Karapiperis <panagiotis_karapiperis@hotmail.com>
2023-09-20 12:53:12 +0300

    Panagiotis Karapiperis ex2

--- src/main/java/com/booleanuk/core/Article.java
+++ src/main/java/com/booleanuk/core/Article.java
@@ -2,9 +2,13 @@ package com.booleanuk.core;
 
 import com.booleanuk.core.example.Item;
 
-public class Article extends Item {
+public class Article extends Author {
 
-    public Article(String title) {
+
+    public Article(String title){
         super(title);
+    }
+    public Article(String title,String authorName,String authorContact,String authorWebsite) {
+        super(title,authorName,authorContact,authorWebsite);
     }
 }
--- src/main/java/com/booleanuk/core/Book.java
+++ src/main/java/com/booleanuk/core/Book.java
@@ -2,9 +2,11 @@ package com.booleanuk.core;
 
 import com.booleanuk.core.example.Item;
 
-public class Book extends Item {
+public class Book extends Author {
     public Book(String title) {
         super(title);
     }
-
+    public Book(String title,String authorName,String authorContact,String authorWebsite) {
+        super(title,authorName,authorContact,authorWebsite);
+    }
 }
