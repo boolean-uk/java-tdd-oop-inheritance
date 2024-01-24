525d7f5f4ae285fc22fa89722dba80ec78343c36
Giar Rehani <giarreh1@gmail.com>
2024-01-24 10:25:53 +0100

    Added library tests

--- src/main/java/com/booleanuk/core/Library.java
+++ src/main/java/com/booleanuk/core/Library.java
@@ -1,5 +1,6 @@
 package com.booleanuk.core;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Library {
@@ -7,12 +8,13 @@ public class Library {
 
 
     public void addToStock(Item item) {
+        this.items = new ArrayList<>();
         this.items.add(item);
     }
     // The following methods may contain code that you are unfamiliar with. The strange syntax of article -> something
     // is called a lambda expression (https://www.w3schools.com/java/java_lambda.asp)
     public String checkInItem(String title) {
-        List<Item> filtered = this.items.stream()
+        List<Item> filtered = items.stream()
                 .filter(item -> item.title.equals(title))
                 .toList();
 
