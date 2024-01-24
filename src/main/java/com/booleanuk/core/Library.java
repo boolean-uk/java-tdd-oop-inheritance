28251ca74b99de08bafcb0eeb60244c574e7b61c
Marit <marit.moe2@ntebb.no>
2024-01-24 09:55:46 +0100

    Used the inheritance to remove repetition.

--- src/main/java/com/booleanuk/core/Library.java
+++ src/main/java/com/booleanuk/core/Library.java
@@ -3,27 +3,17 @@ package com.booleanuk.core;
 import java.util.List;
 
 public class Library {
-    List<Article> articles;
-    List<Book> books;
-    List<Newspaper> newspapers;
+    List<Item> items;
 
-    public void addToStock(Article item) {
-        this.articles.add(item);
-    }
-
-    public void addToStock(Book item) {
-        this.books.add(item);
-    }
-
-    public void addToStock(Newspaper item) {
-        this.newspapers.add(item);
+    public void addToStock(Item item) {
+        this.items.add(item);
     }
 
     // The following methods may contain code that you are unfamiliar with. The strange syntax of article -> something
     // is called a lambda expression (https://www.w3schools.com/java/java_lambda.asp)
-    public String checkInArticle(String title) {
-        List<Article> filtered = this.articles.stream()
-                .filter(article -> article.title.equals(title))
+    public String checkInItem(String title) {
+        List<Item> filtered = this.items.stream()
+                .filter(item -> item.title.equals(title))
                 .toList();
 
         if (filtered.size() < 1) {
@@ -33,9 +23,9 @@ public class Library {
         return filtered.get(0).checkIn();
     }
 
-    public String checkOutArticle(String title) {
-        List<Article> filtered = this.articles.stream()
-                .filter(article -> article.title.equals(title))
+    public String checkOutItem(String title) {
+        List<Item> filtered = this.items.stream()
+                .filter(item -> item.title.equals(title))
                 .toList();
 
         if (filtered.size() < 1) {
@@ -45,51 +35,4 @@ public class Library {
         return filtered.get(0).checkOut();
     }
 
-    public String checkInBook(String title) {
-        List<Book> filtered = this.books.stream()
-                .filter(book -> book.title.equals(title))
-                .toList();
-
-        if (filtered.size() < 1) {
-            return "item is not part of the library's collection";
-        }
-
-        return filtered.get(0).checkIn();
-    }
-
-    public String checkOutBook(String title) {
-        List<Book> filtered = this.books.stream()
-                .filter(book -> book.title.equals(title))
-                .toList();
-
-        if (filtered.size() < 1) {
-            return "item is not part of the library's collection";
-        }
-
-        return filtered.get(0).checkOut();
-    }
-
-    public String checkInNewspaper(String title) {
-        List<Newspaper> filtered = this.newspapers.stream()
-                .filter(newspaper -> newspaper.title.equals(title))
-                .toList();
-
-        if (filtered.size() < 1) {
-            return "item is not part of the library's collection";
-        }
-
-        return filtered.get(0).checkIn();
-    }
-
-    public String checkOutNewspaper(String title) {
-        List<Newspaper> filtered = this.newspapers.stream()
-                .filter(newspaper -> newspaper.title.equals(title))
-                .toList();
-
-        if (filtered.size() < 1) {
-            return "item is not part of the library's collection";
-        }
-
-        return filtered.get(0).checkOut();
-    }
 }
