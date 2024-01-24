9adf057390531d7e2ef4dbbf2ee4937762454189
Marit <marit.moe2@ntebb.no>
2024-01-24 10:38:51 +0100

    Tests already passing.

--- src/test/java/com/booleanuk/core/LibraryTest.java
+++ src/test/java/com/booleanuk/core/LibraryTest.java
@@ -28,4 +28,62 @@ public class LibraryTest {
 
         Assertions.assertEquals(4, library.items.size());
     }
+
+    @Test
+    public void shouldCheckOutIfAvailable() {
+        Library library = new Library();
+        Item article = new Article("Java Rocks");
+        Item book = new Book("Java for Beginners");
+        Item newspaper = new Newspaper("Java Newspaper");
+        library.addToStock(article);
+        library.addToStock(book);
+        library.addToStock(newspaper);
+
+        Assertions.assertEquals("item has been checked out", article.checkOut());
+        Assertions.assertEquals("item has been checked out", book.checkOut());
+        Assertions.assertEquals("newspapers are not available for loan", newspaper.checkOut());
+    }
+
+    @Test
+    public void shouldDeclineIfNotAvailableToCheckout() {
+        Library library = new Library();
+        Item article = new Article("Java Rocks");
+        Item book = new Book("Java for Beginners");
+        library.addToStock(article);
+        library.addToStock(book);
+        article.checkOut();
+        book.checkOut();
+
+        Assertions.assertEquals("item is currently on loan", article.checkOut());
+        Assertions.assertEquals("item is currently on loan", book.checkOut());
+    }
+
+    @Test
+    public void shouldCheckInIfOnLoan() {
+        Library library = new Library();
+        Item article = new Article("Java Rocks");
+        Item book = new Book("Java for Beginners");
+        Item newspaper = new Newspaper("Java Newspaper");
+        library.addToStock(article);
+        library.addToStock(book);
+        library.addToStock(newspaper);
+        article.checkOut();
+        book.checkOut();
+
+        Assertions.assertEquals("item has been checked in", article.checkIn());
+        Assertions.assertEquals("item has been checked in", book.checkIn());
+        Assertions.assertEquals("newspapers are not available for loan", newspaper.checkIn());
+    }
+
+    @Test
+    public void shouldDeclineCheckInIfNotOnLoan() {
+        Library library = new Library();
+        Item article = new Article("Java Rocks");
+        Item book = new Book("Java for Beginners");
+        library.addToStock(article);
+        library.addToStock(book);
+
+        Assertions.assertEquals("item is not currently on loan", article.checkIn());
+        Assertions.assertEquals("item is not currently on loan", book.checkIn());
+    }
 }
