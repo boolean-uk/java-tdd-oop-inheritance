50fae817111b77390cdb48e2f28acbfc9e182d6b
Nicolai Klokmose <angryniczorn@gmail.com>
2024-01-24 10:53:52 +0100

    Kek

--- src/main/java/com/booleanuk/core/Article.java
+++ src/main/java/com/booleanuk/core/Article.java
@@ -1,35 +1,24 @@
 package com.booleanuk.core;
 
-public class Article {
-    String title;
-
+public class Article extends Item {
+    protected String title;
+    protected Author author;
     boolean onLoan = false;
 
     public Article(String title) {
-        this.title = title;
+        super(title);
     }
 
-    public boolean isOnLoan() {
-        return onLoan;
+    public Article(String title, Author author) {
+        super(title);
+        this.setAuthor(author);
     }
 
-    public String checkIn() {
-        if (!this.isOnLoan()) {
-            return "item is not currently on loan";
-        }
-
-        this.onLoan = false;
-
-        return "item has been checked in";
+    public Author getAuthor() {
+        return author;
     }
 
-    public String checkOut() {
-        if (this.isOnLoan()) {
-            return "item is currently on loan";
-        }
-
-        this.onLoan = true;
-
-        return "item has been checked out";
+    public void setAuthor(Author author) {
+        this.author = author;
     }
 }
--- src/main/java/com/booleanuk/core/Book.java
+++ src/main/java/com/booleanuk/core/Book.java
@@ -1,35 +1,24 @@
 package com.booleanuk.core;
 
-public class Book {
-    String title;
-
+public class Book extends Item {
+    protected String title;
+    protected Author author;
     boolean onLoan = false;
 
     public Book(String title) {
-        this.title = title;
+        super(title);
     }
 
-    public boolean isOnLoan() {
-        return onLoan;
+    public Book(String title, Author author) {
+        super(title);
+        this.setAuthor(author);
     }
 
-    public String checkIn() {
-        if (!this.isOnLoan()) {
-            return "item is not currently on loan";
-        }
-
-        this.onLoan = false;
-
-        return "item has been checked in";
+    public Author getAuthor() {
+        return author;
     }
 
-    public String checkOut() {
-        if (this.isOnLoan()) {
-            return "item is currently on loan";
-        }
-
-        this.onLoan = true;
-
-        return "item has been checked out";
+    public void setAuthor(Author author) {
+        this.author = author;
     }
 }
