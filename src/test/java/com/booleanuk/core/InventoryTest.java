e71ed4fb1ddc75f6fee5ea7bd5aa1121df61f4a8
ThomasKvam <thomas.e.h.kvam@hotmail.no>
2024-01-24 10:09:31 +0100

    Added tests to inventory and changed library to use less methods. Added tests to library

--- src/test/java/com/booleanuk/core/InventoryTest.java
+++ src/test/java/com/booleanuk/core/InventoryTest.java
@@ -1,4 +1,52 @@
 package com.booleanuk.core;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class InventoryTest {
+
+    @Test
+    public void testInventoryCheckOut(){
+        Inventory inventory = new Inventory("Moby dick");
+        Inventory article = new Article("Medicine advancements");
+        Assertions.assertEquals("item has been checked out",inventory.checkOut());
+        Assertions.assertEquals("item has been checked out", article.checkOut());
+    }
+
+    @Test
+    public void testInventoryCheckOutFail(){
+        Inventory book = new Book("Moby dick");
+        Inventory article = new Article("");
+        Inventory newspaper = new Newspaper("New York Times");
+        book.checkOut();
+        article.checkOut();
+        newspaper.checkOut();
+        Assertions.assertEquals("item is currently on loan",book.checkOut());
+        Assertions.assertEquals("item is currently on loan", article.checkOut());
+        Assertions.assertEquals("newspapers are not available for loan", newspaper.checkOut());
+    }
+
+    @Test
+    public void shouldCheckInIfOnLoan() {
+        Inventory article = new Article("JUnit Rocks");
+        Inventory book = new Book("Moby Dick");
+        Inventory newspaper = new Newspaper("Medicine advancements");
+        article.checkOut();
+        book.checkOut();
+        newspaper.checkOut();
+        Assertions.assertEquals("item has been checked in", article.checkIn());
+        Assertions.assertEquals("item has been checked in", book.checkIn());
+        Assertions.assertEquals("newspapers are not available for loan", newspaper.checkIn());
+    }
+
+    @Test
+    public void shouldDeclineCheckInIfNotOnLoan() {
+        Inventory article = new Article("JUnit Rocks");
+        Inventory book = new Book("Moby Dick");
+        Inventory newspaper = new Newspaper("Medicine advancements");
+        Assertions.assertEquals("item is not currently on loan", article.checkIn());
+        Assertions.assertEquals("item is not currently on loan", book.checkIn());
+        Assertions.assertEquals("newspapers are not available for loan", newspaper.checkIn());
+
+    }
 }
