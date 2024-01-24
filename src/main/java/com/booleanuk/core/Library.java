8e8672dcca3d0c97761f062d0da69ddb0c71ee8d
Nikolaos Litsas <litsasnik@outlook.com>
2024-01-24 12:24:00 +0200

    Nikolaos Litsas

--- src/main/java/com/booleanuk/core/Library.java
+++ src/main/java/com/booleanuk/core/Library.java
@@ -8,6 +8,9 @@ public class Library {
     public void addToStock(Item item) {
         this.items.add(item);
     }
+    public int seeList() {
+        return items.size();
+    }
 
     // The following methods may contain code that you are unfamiliar with. The strange syntax of article -> something
     // is called a lambda expression (https://www.w3schools.com/java/java_lambda.asp)
--- src/test/java/com/booleanuk/core/LibraryTest.java
+++ src/test/java/com/booleanuk/core/LibraryTest.java
@@ -1,5 +1,19 @@
 package com.booleanuk.core;
 
-public class LibraryTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
+public class LibraryTest {
+    @Test
+    public void addStockTest() {
+        Library library = new Library();
+        Book book = new Book("book1");
+        Article article = new Article("article1");
+        library.addToStock(book);
+        Assertions.assertEquals(1, library.seeList());
+        library.addToStock(article);
+        Assertions.assertEquals(2, library.seeList());
+    }
+    //@Test
+    //public void
 }
