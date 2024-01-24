6a001932a1ece8db3bed14bc8843a4f18b7a6cf6
Osamah Al-maliki <osamah.al-maliki@no.experis.com>
2024-01-24 10:52:53 +0100

    Finished exercise 2

--- src/main/java/com/booleanuk/core/Article.java
+++ src/main/java/com/booleanuk/core/Article.java
@@ -1,8 +1,22 @@
 package com.booleanuk.core;
 
 public class Article extends ReadingMaterial {
+    Author author;
 
     public Article(String title) {
         super(title);
+    }
+
+    public Article(String title, Author author) {
+        super(title);
+        this.author = author;
+    }
+
+    public String getAuthorName() {
+        if (author != null) {
+            return "The author is " + author.getName();
+        } else {
+            return "This reading material has no author";
+        }
     }
 }
--- src/main/java/com/booleanuk/core/Book.java
+++ src/main/java/com/booleanuk/core/Book.java
@@ -1,8 +1,22 @@
 package com.booleanuk.core;
 
 public class Book extends ReadingMaterial {
+    Author author;
 
     public Book(String title) {
         super(title);
+    }
+
+    public Book(String title, Author author) {
+        super(title);
+        this.author = author;
+    }
+
+    public String getName() {
+        if (author != null) {
+            return "The author is " + author.getName();
+        } else {
+            return "This reading material has no author";
+        }
     }
 }
