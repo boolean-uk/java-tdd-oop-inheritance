92ebefe02b2c251f9b36951542ada05f87297a1a
Sandersaether <sander.sather@gmail.com>
2024-01-24 11:00:03 +0100

    Exercise 1

--- src/main/java/com/booleanuk/core/Book.java
+++ src/main/java/com/booleanuk/core/Book.java
@@ -1,35 +1,12 @@
 package com.booleanuk.core;
 
-public class Book {
-    String title;
+public class Book extends Stock {
 
-    boolean onLoan = false;
 
     public Book(String title) {
-        this.title = title;
+        super(title);
     }
 
-    public boolean isOnLoan() {
-        return onLoan;
-    }
 
-    public String checkIn() {
-        if (!this.isOnLoan()) {
-            return "item is not currently on loan";
-        }
 
-        this.onLoan = false;
-
-        return "item has been checked in";
-    }
-
-    public String checkOut() {
-        if (this.isOnLoan()) {
-            return "item is currently on loan";
-        }
-
-        this.onLoan = true;
-
-        return "item has been checked out";
-    }
 }
